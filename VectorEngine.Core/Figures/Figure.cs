534e03228c93fc6ca7348fe780b435c999074e8f
Denis Obolensky <denismaster@outlook.com>
2015-02-18 23:22:58 +0300

    [18.02.15] -Added Figure and Vector Object classes -Added JSON and Direct2D nuget packages to solution

---
+++ VectorEngine.Core/Figures/Figure.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpDX;
+using SharpDX.Direct2D1;
+
+namespace VectorEngine.Core.Figures
+{
+    public abstract class Figure : VectorObject
+    {
+        public abstract bool IsPointInside(Point p);
+
+        public static Factory D2DFactory
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ VectorEngine.Core/Figures/VectorObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorEngine.Core.Figures
+{
+    //Представляет возможность работы с объектами как с объектами VectorEditor
+    public abstract class VectorObject
+    {
+        string _name = String.Empty;
+        String Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if(!String.IsNullOrEmpty(value))
+                {
+                    _name = value;
+                }
+            }
+        }
+    }
+}
