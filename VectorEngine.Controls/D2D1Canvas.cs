ed64d82deb5499444bc9dc37146222db3b209345
Denis Obolensky <denismaster@outlook.com>
2015-03-01 16:17:44 +0300

    [01.03.2015] -Added D2DLayer and D2DComponent -Added Win project for testing and publishing -Added some classes and interfaces

---
+++ VectorEngine.Controls/D2D1Canvas.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using SharpDX.Direct2D1;
+using SharpDX;
+using SharpDX.DirectWrite;
+namespace Seal2D.Control
+{
+    public class D2DCanvas : D2DLayer
+    {
+        public D2DCanvas()
+        {
+        }
+        public RenderTarget RenderTarget
+        {
+            get
+            {
+                return this.renderTarget;
+            }
+        }
+        protected override void OnCreateRenderObjects()
+        {
+        }
+        protected override void OnMouseDown(System.Windows.Forms.MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+        }
+        protected override void OnDisposeRenderObjects()
+        {
+        }
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            base.OnMouseUp(e);
+        }
+        protected override void OnUpdate()
+        {
+
+        } 
+        protected override void OnRender()
+        {
+            renderTarget.Clear(Color.White);
+        }
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+        }
+    }
+}
